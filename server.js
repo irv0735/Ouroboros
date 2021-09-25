ee08f5c7fdacc2ed857d88deb9960e8bd7d2481e
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-09-25 13:58:27 -0600

    changed server.js since we weren't using the helpers file

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers')
-const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,7 +10,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 
 const sess = {
   secret: 'Super secret secret',
