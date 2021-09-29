6e99624eec306125b467ae7aaf6ed1eae5ac7598
jkworks <info@jilliankayworks.com>
2021-09-28 21:32:39 -0600

    working progress bar

--- public/js/progress-bar.js
+++ public/js/progress-bar.js
@@ -1,19 +1,19 @@
   
-  let number = document.getElementById("number")
+  let number = document.getElementById("number");
   let counter = 0;
  // let points = Progress.exercise;
-  let percentage = document.querySelector('.progress').dataset.percent
+  let percentage = document.querySelector('.progress').dataset.percent;
   console.log(percentage);
 
   setInterval(() => {
     //store users progress points
     //pull this number from db? replace the 65
     //edit css animation
-    // if(counter < percentage){
-    //   clearInterval();
-    // } else {
-    //      counter += 1;
-    number.innerHTML = percentage + "%";
-    // }
+    if(counter == percentage){
+      clearInterval();
+    } else {
+         counter += 1;
+    number.innerHTML = counter + "%";
+    }
  
   }, 20);
