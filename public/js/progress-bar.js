9912cbaa287915271d10f3f79d071582d3f58448
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-10-01 10:18:14 -0600

    Updated progress circles to show the percentage in each one

--- public/js/progress-bar.js
+++ public/js/progress-bar.js
@@ -1,18 +1,34 @@
   
-  let number = document.getElementById("number");
-  let counter = 0;
+  // let number = document.getElementById("number");
+  // let counter = 0;
 
-  let percentage = document.querySelector('.progress').dataset.percent;
+  // let percentage = document.querySelector('.progress').dataset.percent;
 
-  setInterval(() => {
-    //store users progress points
-    //pull this number from db? replace the 65
-    //edit css animation
-    if(counter == percentage){
-      clearInterval();
-    } else {
-         counter += 1;
-    number.innerHTML = counter + "%";
-    }
+  // setInterval(() => {
+  //   //store users progress points
+  //   //pull this number from db? replace the 65
+  //   //edit css animation
+  //   if(counter == percentage){
+  //     clearInterval();
+  //   } else {
+  //        counter += 1;
+  //   number.innerHTML = counter + "%";
+  //   }
  
-  }, 20);
+  // }, 20);
+
+
+  const innerNumbers = document.querySelectorAll('#number');
+
+  innerNumbers.forEach(element => {
+    let counter = 0;
+    let percentage = element.closest('.progress').dataset.percent;
+    setInterval( () => {
+      if (counter == percentage) {
+        clearInterval();
+      } else {
+        counter += 1;
+        element.innerHTML = counter + '%';
+      }
+    }, 20);
+  });
