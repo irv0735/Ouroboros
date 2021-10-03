1ffa0e23f57eca46f64108879f8143e8313fe20e
Micheal Limones <michlimones@gmail.com>
2021-10-03 17:20:02 -0600

    added animation to header to fade in.

---
+++ public/js/animation.js
@@ -0,0 +1,22 @@
+var textWrapper = document.querySelector('h1');
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: 'h1 .letter',
+    opacity: [0, 1],
+    easing: 'easeInOutQuad',
+    duration: 2250,
+    delay: (el, i) => 150 * (i + 1),
+  })
+  .add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: 'easeOutExpo',
+    delay: 1000,
+  });
