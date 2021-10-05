5e5c1ec2f8def37452320d31aa9ea3714b670620
Micheal Limones <michlimones@gmail.com>
2021-10-05 11:41:57 -0600

    centered bio, goals, and buttons on dashboard. moved quote to bottom of page and styled.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,49 +1,43 @@
 // const suggestionDiv = document.getElementById('suggestion-content');
-//fetch ('/activity-log/activity-count/:id') multiple count by 10 and have their total points 
+//fetch ('/activity-log/activity-count/:id') multiple count by 10 and have their total points
 
 // const { response } = require("express");
 var randomQuoteDiv = document.getElementById('quoteDiv');
-var refreshButton = document.getElementById('refresh')
-fetch("https://type.fit/api/quotes")
-  .then(function(response) {
+var refreshButton = document.getElementById('refresh');
+fetch('https://type.fit/api/quotes')
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data) {
+  .then(function (data) {
     console.log(data[0]);
     // let quote = JSON.stringify(data);
-    let randomQuote = Math.floor(Math.random()*data.length);
+    let randomQuote = Math.floor(Math.random() * data.length);
     let randomText = data[randomQuote].text;
     let randomAuthor = data[randomQuote].author;
     console.log(randomText);
     console.log(randomAuthor);
-   randomQuoteDiv.innerHTML = randomText + "-" + randomAuthor;
-    
+    randomQuoteDiv.innerHTML = `"${randomText}" - ${randomAuthor}`;
   });
 
+const innerNumbers = document.querySelectorAll('#number');
 
-  const innerNumbers = document.querySelectorAll('#number');
+innerNumbers.forEach((element) => {
+  let counter = 0;
+  let percentage = element.closest('.progress').dataset.percent;
+  setInterval(() => {
+    if (counter == percentage) {
+      clearInterval();
+    } else {
+      counter += 1;
+      element.innerHTML = counter + '%';
+    }
+  }, 20);
 
-  innerNumbers.forEach(element => {
-    let counter = 0;
-    let percentage = element.closest('.progress').dataset.percent;
-    setInterval( () => {
-      if (counter == percentage) {
-        clearInterval();
-      } else {
-        counter += 1;
-        element.innerHTML = counter + '%';
-      }
-    }, 20);
+  // if (element< 50) {
+  //   const creativeDiv = document.innerHTML.suggestionDiv =  + " your world might not feel very colorful today may we suggest doing something creative?"
+  //             "Maybe write in a journal, create an en plein air painting or go to the museum."
 
-    // if (element< 50) {
-    //   const creativeDiv = document.innerHTML.suggestionDiv =  + " your world might not feel very colorful today may we suggest doing something creative?"
-    //             "Maybe write in a journal, create an en plein air painting or go to the museum."
-            
-    //             const movementDiv = document.innerHTML.suggestionDiv = "GET YA ASS UP "
+  //             const movementDiv = document.innerHTML.suggestionDiv = "GET YA ASS UP "
 
-    // }
-  });
-
-
-
-
+  // }
+});
