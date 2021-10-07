f064917d2ee8c67df9ba8dbe420f7b6833c75cde
jkworks <info@jilliankayworks.com>
2021-10-07 11:12:09 -0600

    working btn need styling

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,12 +1,13 @@
-var btn_up = document.querySelector('.liked');
-var btn_down = document.querySelector('.notLiked');
+var liked = document.querySelector('.liked');
+var notLiked = document.querySelector('.notLiked');
 
-function changeImage(elem){
-    elem.classList.toggle('clicked');
-    if(elem === btn_up){
-        btn_down.classList.remove('clicked');
-    }else{
-        btn_up.classList.remove('clicked');
+function changeImage() {
+   
+    if (notLiked.src.match("/images/liked.png")) {
+        console.log("/images/liked.png");
+        notLiked.src = "/images/notliked.png" 
+    }
+    else {
+        notLiked.src = "/images/liked.png";
     }
 }
-
