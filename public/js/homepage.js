1ce57d617d555dd1947007df9785b722086e723f
jkworks <info@jilliankayworks.com>
2021-10-07 11:23:16 -0600

    btn no bg need sizing

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -4,7 +4,7 @@ var notLiked = document.querySelector('.notLiked');
 function changeImage() {
    
     if (notLiked.src.match("/images/liked.png")) {
-        console.log("/images/liked.png");
+        
         notLiked.src = "/images/notliked.png" 
     }
     else {
